6678be9c97bd88a8cb5d967b72f6593d85f639b3
isi <isidoor.pinilloesquivel@student.uanwerpen.be>
2022-11-04 11:28:12 +0100

    created a start up script for the public pcs on the university

---
+++ startPublicPc.sh
@@ -0,0 +1,5 @@
+git config --global credential.helper "cache --timeout=${1=7200}"
+git config --global user.name "isi"
+git config --global user.email "isidoor.pinilloesquivel@student.uanwerpen.be"
+
+firefox https://bitwarden.com  https://github.com  https://uantwerpen.be&
