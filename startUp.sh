8907ec748b0375a6c2967fb3dc16b854fc68bb2b
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2022-11-14 12:52:26 +0100

    added PlugInstall on start up and linking to snippets

--- startUp.sh
+++ startUp.sh
@@ -1,9 +1,12 @@
-
 ln -sf ~/.dotfiles/.vimrc .vimrc
 ln -sf ~/.dotfiles/.vim .vim
 ln -sf ~/.dotfiles/.gitconfig .gitconfig
 
+vim -c "source .dotfiles/PlugInstall.vim"
+rm -rf ./dotfiles/.vim/plugged/vim-easycomplete/snippets/ultisnips
+ln -sf ./dotfiles/.vim/plugged/Ultisnips ./dotfiles/.vim/plugged/vim-easycomplete/snippets/ultisnips
+
 firefox about:preferences#search https://bitwarden.com  https://github.com  https://uantwerpen.be&
 
-set -o vi
+
 EDITOR=vim
