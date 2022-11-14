b0f031b482d4ed23947608cd922f6cfb07318354
isi <isidoor.pinilloesquivel@student.uantwerpen.be>
2022-11-14 13:07:00 +0100

    fixed linking snippets

--- startUp.sh
+++ startUp.sh
@@ -4,7 +4,7 @@ ln -sf ~/.dotfiles/.gitconfig .gitconfig
 
 vim 
 rm -rf ~/.dotfiles/.vim/plugged/vim-easycomplete/snippets/ultisnips
-ln -sf ~/.dotfiles/.vim/plugged/Ultisnips ~/.dotfiles/.vim/plugged/vim-easycomplete/snippets/ultisnips
+ln -sf ~/.dotfiles/.vim/Ultisnips ~/.dotfiles/.vim/plugged/vim-easycomplete/snippets/ultisnips
 
 firefox about:preferences#search https://bitwarden.com  https://github.com  https://uantwerpen.be&
 
